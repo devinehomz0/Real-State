8a2f5cdbafd4aa3360af80d8d45a34fd664d8a0a
devronic-tech <devronic.org@gmail.com>
2026-03-24 23:29:22 +0530

    update

--- src/components/ui_Componets/Partners.jsx
+++ src/components/ui_Componets/Partners.jsx
@@ -1,20 +1,13 @@
 import React from "react";
 import "../styles/Partners.css";
-import hdfc from "../../assets/logos/hdfc.png";
-import sbi from "../../assets/logos/sbi.png";
-import axis from "../../assets/logos/axis.jpg";
+
 import insta from "../../assets/logos/insta.webp";
 import face from "../../assets/logos/facebook.webp";
 import yt from "../../assets/logos/youtube.webp";
 import link from "../../assets/logos/linkdin.jpg";
 
 const PartnersShowcase = () => {
-  // Banking partners data
-  const bankingPartners = [
-    { name: "Axis Bank", logo: axis },
-    { name: "HDFC Bank", logo: hdfc },
-    { name: "SBI", logo: sbi },
-  ];
+
 
   // Marketing partners data
   const marketingPartners = [
@@ -42,15 +35,6 @@ const PartnersShowcase = () => {
 
   return (
     <div className="partners-showcase">
-      <div className="partners-section">
-        <h2 className="section-title">Banking Partners</h2>
-        <div className="partners-container">
-          <div className="partners-scroll">
-            {createDuplicatedLogos(bankingPartners, "banking")}
-          </div>
-        </div>
-      </div>
-
       <div className="partners-section">
         <h2 className="section-title">Marketing Partners</h2>
         <div className="partners-container">
